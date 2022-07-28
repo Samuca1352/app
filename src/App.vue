15cc8cbe8d6bcd8b477dfc887ccd8a36f9591217
Samuca1352 <samukael48@gmail.com>
2022-07-28 18:35:52 -0300

    Vue js 3: v-for

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,27 @@
 <template>
   <div class="">
-    <TheHeader
-    v-show="showHeader">
-
-    </TheHeader>
-  <div v-if="level === 'admin'">
-    Admin
-  </div>
-  <div v-else-if="level === 'marketing'">
-  Marketing
-  </div>
-<div v-else>
-  Normal
-</div>
+    <TheHeader>
+  </TheHeader>
 
 
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 import TheHeader from './components/TheHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    TheHeader //tem que importar e declarar em components
-  },
-  data() {
-    return{
-      showHeader: true,
-      name: 'Samuel',
-      level: 'normal'
 
-    }
+    TheHeader //tem que importar e declarar em components
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
