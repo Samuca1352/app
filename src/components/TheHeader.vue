8f1d04d62b541601babf37e14034a6247ccb4693
Samuca1352 <samukael48@gmail.com>
2022-07-28 20:43:27 -0300

    Vue js 3: Eventos - diretiva v-on

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,51 +1,19 @@
 <template>
   <div>
     <div>
-      Two way data bidding v-model -> formularios <br> <br><br>
-      <label for="">Nome</label> <br>
-      <input v-model="name" type="text" name="" id="" /><br>
-      {{name}}
-    </div>
- <br><br> <br><br><br>
-    <div class="">
-        <label for="">Sports</label> <br>
-        <select v-model="sports" name="" id="">
-        <option value="Teste">Teste</option>
-        <option value="Futebol">Futebol</option>
-        <option value="Tenis">Tenis</option>
-        <option value="Volei">Volei</option>
-        </select> <br>
-        {{sports}} <br>
-        </div>
-
-<div>
-    <label for="">Newsletter</label> <br>
-    <input v-model="news" type="radio" value="sim" id="">Sim <br>
-    <input v-model="news" type="radio" value="nao" id="">Nop <br>
-    {{news}}
-</div> <br><br><br>
-<div class="">
-    <div class="form-check">
-      <input v-model="sportes" class="form-check-input" type="checkbox" value="Futebol" id="">
-      <label class="form-check-label" for="">
-        Futebol
-      </label>
-    </div>
-    <div class="form-check">
-      <input v-model="sportes" class="form-check-input" type="checkbox" value="Tenis" id="" >
-      <label class="form-check-label" for="">
-        Tenis
-      </label>
-    </div>
-        <div class="form-check">
-      <input v-model="sportes" class="form-check-input" type="checkbox" value="Volei" id="" >
-      <label class="form-check-label" for="">
-        Volei
-      </label>
-      <br>
-    {{sportes}}
-    </div>
+        <button @click.stop.prevent="onclick">
+            Enviar
+        </button>
 </div>
+<div @mouseover="onMouseover" @mouseout="onMouseout">
+    Mouse over
+</div>
+
+    <form action="google.com"
+     @submit.prevent="onSubmit">
+     <input @keyup="keyUp" type="text">
+     <button type="submit">Submit</button>
+     </form>
 
   </div>
 </template>
@@ -54,13 +22,29 @@
 export default {
   data() {
     return {
-      var: "",
       name: 'Samuel',
-      sports: '',
-      news: 'sim',
-      sportes: [],
-    };
+      
+    }
   },
+
+  methods:{
+    onclick(){
+        console.log('click');
+    },
+    onMouseover(){
+        console.log('mouse over')
+    },
+    onMouseout(){
+        console.log('mouse out')
+    },
+    onSubmit(){
+        console.log('submit')
+    },
+    keyUp($evt){
+        console.log('onKeyUp',$evt)
+    }
+  }
+
 };
 </script>
 
