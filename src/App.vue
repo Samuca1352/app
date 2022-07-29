b29322f8db5cf8ad43f443c42bca30bca914eff9
Samuca1352 <samukael48@gmail.com>
2022-07-29 19:11:51 -0300

    Vue js 3: Router parte 2

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
+    <router-link to="/usuarios/10">Usuário</router-link> |
     <router-link :to="{name: 'servicos'}">Serviços</router-link>
 
   </nav>
