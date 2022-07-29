dabf32340224619c9cc54d6740e2bf1c06e72c2d
Samuca1352 <samukael48@gmail.com>
2022-07-29 11:13:02 -0300

    Vua js 3: Socped e Global css

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div class="">
-    <TheHeader>
-      <template v-slot:title >
-      Home
-      </template>
-      <template v-slot:dsc>
-      <p>Teste</p>
-      </template>
-  </TheHeader>
-  
-
-
-
-  
+    <BaseCard>
+    </BaseCard>
+    <div class="card">
+      Teste
+    </div>
   </div>
 </template>
 
 <script>
 
-import TheHeader from './components/TheHeader.vue';
+import BaseCard from '@/components/BaseCard';
+
 
 export default {
   name: 'App',
   components: {
-
-    TheHeader //tem que importar e declarar em components
+    BaseCard
+    
   },
 
 
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        lipsum teste tset
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.card{
+    background-color: bisque;
+    padding: 10px;
+    border-radius: 10px;
+    font-size: larger;
+}
+</style>
